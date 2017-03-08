671b47cd6b1373d2dcd2220f647263d81b0d776c
Henry Au <henryau01@gmail.com>
2017-03-08 13:29:03 -0800

    animate colors & start creating particles

---
+++ Particle.cpp
@@ -0,0 +1,18 @@
+#include "Particle.h"
+#include <stdlib.h>
+
+namespace particles{
+
+Particle::Particle() {
+    m_x = ((2.0 * rand())/RAND_MAX) - 1;
+    m_y = ((2.0 * rand())/RAND_MAX) - 1;
+}
+
+Particle::~Particle() {
+
+}
+
+} // end namespace particles
+
+
+
--- main.cpp
+++ main.cpp
@@ -2,12 +2,17 @@
 #include <SDL2/SDL.h>
 #include <stdio.h>
 #include "Screen.h"
+#include <math.h>
+#include <stdlib.h>
+#include <time.h>
 
 using namespace std;
 using namespace particles;
 
 int main() {
   
+    srand(time(NULL));
+    
     Screen screen;
 
     if(screen.init() == false) {
@@ -19,13 +24,16 @@ int main() {
         // update particles
 
         // draw particles
+        int elapsed = SDL_GetTicks();
+        unsigned char red = (unsigned char)((1 + sin(elapsed * 0.0007)) * 128);
+        unsigned char green = (unsigned char)((1 + cos(elapsed * 0.0008)) * 128);
+        unsigned char blue = (unsigned char)((1 + sin(elapsed * 0.0009)) * 128);
+
         for(int y = 0; y < Screen::SCREEN_HEIGHT; y++) {
             for(int x = 0; x < Screen::SCREEN_WIDTH; x++) {
-                screen.setPixel(x, y, 128, 0, 255);
+                screen.setPixel(x, y, red, green, blue);
             }
         }
-
-        // screen.setPixel(400, 300, 255, 255, 255);
 
         // draw screen
         screen.update();
